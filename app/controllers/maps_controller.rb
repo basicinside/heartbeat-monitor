84840b657fa9acf327479d13d9d1af934f75a892
Robin Kuck <robin@basicinside.de>
2012-06-23 08:23:38 +0200

    [karte] JSON Map: Node+Links

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,37 @@
 class MapsController < ApplicationController
   def index
-	  render :layout => false
-	end
-end
+    render :layout => false
+  end
+  def map
+    last_seen = Date.today - 7.days
+    nodes = []
+    links = []
+
+    Node.find(:all, :conditions => ["last_seen > '#{last_seen}'"]).each { |node|
+      nodes << { 
+        :id => node.id,
+        :name => node.name, 
+          :lat => node.lat,
+          :lon => node.lon,
+          :ipv4 => node.default_ipv4
+      }
+    }
+
+    Link.find(:all, :conditions => ["last_seen > '#{last_seen}'"]).each { |link|
+      begin
+        links << {
+        :from => link.node1,
+        :to => link.node2,
+        :lq => link.lq,
+        :nlq => link.nlq
+      }
+      rescue
+      end
+    }
+
+    @nodes = nodes.to_json
+    @links = links.to_json
+
+    render :layout => false
+   end
+  end
